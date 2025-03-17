8293a256fea56d23f3b2c5db5f70d1817c36e75a
Carlos Stekel Valencia <cstekel@mbi.local>
2025-03-17 09:52:51 -0300

    arreglar bug urls

--- API_Operaciones/operaciones/urls.py
+++ API_Operaciones/operaciones/urls.py
@@ -30,32 +30,32 @@ urlpatterns = [
         name="custodia",
     ),
     path(
-        "Custodia/consultarMovimientos/<str:documento_identidad>/<str:moneda>/<str:fecha_inicio>/<str:fecha_termino>",
+        "Custodia/consultarMovimientos/<str:documento_identidad>/<str:moneda>/<str:fecha_inicio>/<str:fecha_termino>/",
         ConsultarMovimientosCustodia.as_view(),
         name="movimientosCustodia",
     ),
     path(
-        "Custodia/ConsultaFlujoCustodia/<str:documento_identidad>/<str:codigo_portafolio>/<str:nemotecnico>/<str:tipo_flujo>/<str:fecha_consulta>",
+        "Custodia/ConsultaFlujoCustodia/<str:documento_identidad>/<str:codigo_portafolio>/<str:nemotecnico>/<str:tipo_flujo>/<str:fecha_consulta>/",
         ConsultarFlujoCustodia.as_view(),
         name="flujoCustodia",
     ),
     path(
-        "Custodia/consultarBalanceCustodia/<str:documento_identidad>/<str:codigo_portafolio>/<str:fecha_consulta>",
+        "Custodia/consultarBalanceCustodia/<str:documento_identidad>/<str:codigo_portafolio>/<str:fecha_consulta>/",
         ConsultarBalanceCustodia.as_view(),
         name="balanceCustodia",
     ),
     path(
-        "Custodia/consultarCartera/<str:documento_identidad>/<str:codigo_portafolio>/<str:fecha_consulta>",
+        "Custodia/consultarCartera/<str:documento_identidad>/<str:codigo_portafolio>/<str:fecha_consulta>/",
         ConsultarCartera.as_view(),
         name="cartera",
     ),
     path(
-        "Custodia/consultarEventosCapital/<str:instrumento>/<str:fecha_inicio>/<str:fecha_termino>",
+        "Custodia/consultarEventosCapital/<str:instrumento>/<str:fecha_inicio>/<str:fecha_termino>/",
         ConsultarEventosCapital.as_view(),
         name="eventosCapital",
     ),
     path(
-        "Custodia/consultarEventosCapitalCliente/<str:documento_identidad>",
+        "Custodia/consultarEventosCapitalCliente/<str:documento_identidad>/",
         ConsultarEventosCapitalCliente.as_view(),
         name="eventosCapitalCliente",
     ),
@@ -66,17 +66,17 @@ urlpatterns = [
         name="saldos",
     ),
     path(
-        "CuentaInversion/consultarMovimientos/<str:documento_identidad>/<int:codigo_portafolio>/<str:moneda>/<str:fecha_inicio>/<str:fecha_termino>",
+        "CuentaInversion/consultarMovimientos/<str:documento_identidad>/<int:codigo_portafolio>/<str:moneda>/<str:fecha_inicio>/<str:fecha_termino>/",
         ConsultarMovimientos.as_view(),
         name="movimientos",
     ),
     path(
-        "CuentaInversion/consultarFlujoCaja/<str:documento_identidad>/<str:codigo_portafolio>/<str:liquidacion>/<str:moneda>/<str:uso_flujo_caja>/<str:fecha_inicio>/<str:fecha_termino>",
+        "CuentaInversion/consultarFlujoCaja/<str:documento_identidad>/<str:codigo_portafolio>/<str:liquidacion>/<str:moneda>/<str:uso_flujo_caja>/<str:fecha_inicio>/<str:fecha_termino>/",
         ConsultarFlujoCaja.as_view(),
         name="flujoCaja",
     ),
     path(
-        "CuentaInversion/consultarHistorialMovimientos/<str:documento_identidad>/<str:codigo_portafolio>/<str:moneda>/<str:fecha_inicio>/<str:fecha_termino>",
+        "CuentaInversion/consultarHistorialMovimientos/<str:documento_identidad>/<str:codigo_portafolio>/<str:moneda>/<str:fecha_inicio>/<str:fecha_termino>/",
         ConsultarHistorialMovimientos.as_view(),
         name="historialMovimientos",
     ),
@@ -93,7 +93,7 @@ urlpatterns = [
     ),
     ######################################### PORTAFOLIO ##########################################
     path(
-        "Portafolio/consultarCobros/<str:documento_identidad>/<int:codigo_portafolio>/<str:tipo_instrumento>",
+        "Portafolio/consultarCobros/<str:documento_identidad>/<int:codigo_portafolio>/<str:tipo_instrumento>/",
         CalculoComision.as_view(),
         name="cobros",
     ),
@@ -105,7 +105,7 @@ urlpatterns = [
     ),
     ######################################### FACTURACION #########################################
     path(
-        "Facturacion/consultarComprobantes/<str:documento_identidad>/<str:codigo_portafolio>/<str:fecha_inicio>/<str:fecha_termino>",
+        "Facturacion/consultarComprobantes/<str:documento_identidad>/<str:codigo_portafolio>/<str:fecha_inicio>/<str:fecha_termino>/",
         ConsultarComprobantesFacturacion.as_view(),
         name="comprobantesFacturacion",
     ),
